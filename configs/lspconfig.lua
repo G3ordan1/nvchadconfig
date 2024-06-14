3dfc9acb5b824effa1e73db701ebf8ccc83d5605
G3ordan1 <geordan.ramsamy@gmail.com>
2024-06-14 19:30:33 +0400

    lspconfig for python

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -15,3 +15,32 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.pylsp.setup {
+on_attach = on_attach,
+on_init = on_init,
+settings = {
+    pylsp = {
+    plugins = {
+        -- formatter options
+        black = { enabled = true },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = { enabled = true, executable = "pylint" },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = { enabled = false },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        pyls_isort = { enabled = true },
+    },
+    },
+},
+flags = {
+    debounce_text_changes = 200,
+},
+capabilities = capabilities,
+}
+
